83869ee56938e28d7aa2cf5913dc9d17acc689a1
aria.hur <skyda1204@gmail.com>
2026-01-03 21:43:51 +0900

    update DirectionsSection to use fixed Kakao map link and embed Google Maps with specific location

--- src/sections/DirectionsSection.tsx
+++ src/sections/DirectionsSection.tsx
@@ -124,7 +124,7 @@ const DirectionsSection: React.FC = () => {
 
   const handleKakaoMapClick = () => {
     const searchQuery = encodeURIComponent(t.directions.address);
-    window.open(`https://map.kakao.com/link/search/${searchQuery}`, '_blank');
+    window.open(`https://map.kakao.com/link/to/1948333104`, '_blank');
   };
 
   return (
@@ -204,7 +204,7 @@ const DirectionsSection: React.FC = () => {
             )
           ) : (
             <iframe
-              src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ''}&q=${encodeURIComponent(t.directions.address)}`}
+              src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3165.079789985077!2d127.06414271122055!3d37.50603622747875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca447548b6c33%3A0xe8177a2e737683a9!2sGrand%20Hill%20Convention%2C%20Seoul!5e0!3m2!1sen!2skr!4v1767444168082!5m2!1sen!2skr`}
               width="100%"
               height="250"
               style={{ border: 0, borderRadius: '8px' }}
